6e2b8fbbfc41a16724604ab0424970773789f4df
Jessewhalen1992 <jesse_whalen@hotmail.com>
2025-11-06 16:32:53 -0400

    fixed cogo locations

--- src/Compass/Modules/CogoProgramModule.cs
+++ src/Compass/Modules/CogoProgramModule.cs
@@ -10,7 +10,8 @@ public class CogoProgramModule : ManagedPluginModuleBase
     private static readonly string[] CandidatePaths =
     {
         @"C:\\AUTOCAD-SETUP CG\\CG_LISP\\COMPASS\\COGO PROGRAM\\SurveyCalculator.DLL",
-        @"C:\\AUTOCAD-SETUP\\Lisp_2000\\COMPASS\\COGO PROGRAM\\SurveyCalculator.DLL"
+        @"C:\\AUTOCAD-SETUP CG\\CG_LISP\\COMPASS\\COGO PROGRAM (2015)\\SurveyCalculator.DLL",
+        @"C:\\AUTOCAD-SETUP\\Lisp_2000\\COMPASS\\COGO PROGRAM (2015)\\SurveyCalculator.DLL"
     };
 
     public override string Id => "cogo-ui";
